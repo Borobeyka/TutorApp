b0e8192ec6d62204e5f005af2a98135c3a4e095d
Borobeyka <dandr07@yandex.ru>
2022-05-21 22:07:25 +0300

    .

--- __main__.py
+++ __main__.py
@@ -6,6 +6,8 @@ from kivy.lang import Builder
 from kivy.config import Config
 from kivy.storage.jsonstore import JsonStore
 
+#
+
 # UIX
 from objects.ScreenManagement import *
 
@@ -13,19 +15,14 @@ from objects.ScreenManagement import *
 Builder.load_file("./objects/widgets/root.kv")
 from objects.widgets.MainWidget import *
 
+#DB = cDatabase()
+
 class MainApp(MDApp):
     def __init__ (self, **kwargs):
         super(MainApp, self).__init__(**kwargs)
         with open("config.json", encoding="utf-8") as file:
             self.settings = json.loads(file.read())
         self.store = JsonStore("data.json")
-        self.con = pymysql.connect(
-            host = self.settings["app"]["db"]["hostname"],
-            user = self.settings["app"]["db"]["username"],
-            password = self.settings["app"]["db"]["password"],
-            database = self.settings["app"]["db"]["database"],
-            charset = "utf8",
-            cursorclass = pymysql.cursors.DictCursor)
         Config.set("graphics", "width", self.settings["app"]["width"])
         Config.set("graphics", "height", self.settings["app"]["height"])
         Config.write()
@@ -33,16 +30,26 @@ class MainApp(MDApp):
     
     def is_logged(self):
         if self.store.exists("data"):
-            with self.con:
-                cur = self.con.cursor()
-                count = cur.execute("SELECT login FROM tutors WHERE login='%s'" % self.store.get("data")["login"])
+            with self.con() as cursor:
+                sql = "SELECT login FROM tutors WHERE login=%s"
+                count = cursor.execute(sql, (self.store.get("data")["login"],))
                 if count != 0:
                     return True
         return False
+    
+    def con(self):
+        con = pymysql.connect(
+            host = self.settings["app"]["db"]["hostname"],
+            user = self.settings["app"]["db"]["username"],
+            password = self.settings["app"]["db"]["password"],
+            database = self.settings["app"]["db"]["database"],
+            charset = "utf8",
+            cursorclass = pymysql.cursors.DictCursor)
+        return con.cursor()
         
     def build(self):
         mw = MainWidget()
-        mw.ids["manager"].get_screen("schedule").load() #####
+        mw.ids["manager"].get_screen("schedule").update() #####
         return mw
 
 if __name__ == "__main__":
--- objects/screens/ScheduleScreen.py
+++ objects/screens/ScheduleScreen.py
@@ -1,5 +1,6 @@
 import datetime
 from dateutil.relativedelta import relativedelta
+from kivymd.app import MDApp
 from kivy.lang import Builder
 
 from kivy.uix.screenmanager import Screen
@@ -12,21 +13,40 @@ class ScheduleScreen(Screen):
     def __init__ (self, **kwargs):
         super(ScheduleScreen, self).__init__(**kwargs)
         self.current_date = datetime.datetime.now()
+        self.app = MDApp.get_running_app()
     
-    def load(self):
+    def update(self):
+        self.update_headers()
         self.update_content()
         
-    def update_content(self):
+    def update_headers(self):
         self.ids["header_month"].text = self.current_date.strftime("%B, %Y")
         self.ids["header_day"].text = self.current_date.strftime("%d, %A")
 
-        print(self.ids["students_list"].add_widget(StudentCard()))
+    def update_content(self):
+        self.ids["students_list"].clear_widgets()
+        with self.app.con() as cursor:
+            sql = """SELECT DISTINCT l.*, s.*, lt.* FROM lessons AS l LEFT JOIN students AS s
+                ON l.student_id = s.id LEFT JOIN lesson_type AS lt ON l.lesson_type_id = lt.id
+                WHERE l.date = %s AND l.tutor_id = (SELECT id FROM tutors WHERE login = %s)"""
+            date = self.current_date.date()
+            cursor.execute(sql, (date, self.app.store.get("data")["login"]))
+            rows = cursor.fetchall()
+            if len(rows) == 0:
+                return
+            for index, row in enumerate(rows, start=1):
+                student_card = StudentCard()
+                student_card.ids["id"].text = str(index)
+                #datetime.strptime(str(row["time"]), "%H/%m/%S").date()
+                student_card.ids["time"].text = str(row["time"])[:-3]
+                student_card.ids["name"].text = row["name"]
+                student_card.ids["address"].text = row["address"]+ " (" + row["lesson_type"] + ")"
+                self.ids["students_list"].add_widget(student_card)
 
-        
     def update_month(self, month):
         self.current_date = self.current_date + relativedelta(months=month)
-        self.update_content()
+        self.update()
     
     def update_day(self, day):
         self.current_date = self.current_date + relativedelta(days=day)
-        self.update_content()
+        self.update()
